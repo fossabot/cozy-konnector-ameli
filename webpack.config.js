90e6f1adfabfb78c174acb0bc3b815a658210a90
Brice Coquereau <brice@coquereau.fr>
2017-10-19 22:07:13 +0200

    Added babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,32 @@ module.exports = {
     path: path.join(__dirname, 'build'),
     filename: 'index.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve('node_modules/cozy-konnector-libs')
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                'targets': {
+                  'node': '6.10'
+                }
+              }]
+            ],
+            plugins: [
+              'transform-object-rest-spread'
+            ]
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new CopyPlugin([
       { from: 'manifest.konnector' },
